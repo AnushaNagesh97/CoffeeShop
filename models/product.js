ef8ea4a7931b5ec16126e0c138dd0b365f239792
Shashwat Nayak <shashwatnayak@outlook.com>
2023-11-28 17:25:15 -0600

    Added routes for products

--- models/product.js
+++ models/product.js
@@ -31,12 +31,23 @@ Product.add = function(product) {
     return sequelize.query(`INSERT INTO products (product_name, price, available_stock, category, image_path, product_description, alt_text) VALUES ('${product.product_name}', ${product.price}, ${product.available_stock}, '${product.category}', '${product.image_path}', '${product.product_description}', '${product.alt_text}')`, { type: Sequelize.QueryTypes.INSERT });
 };
 
-// Product.update = function(id, product) {
-//     return sequelize.query(`UPDATE products SET product_name = '${product.product_name}', price = ${product.price}, available_stock = ${product.available_stock}, category = '${product.category}', image_path = '${product.image_path}', product_description = '${product.product_description}', alt_text = '${product.alt_text}' WHERE product_id = ${id}`, { type: Sequelize.QueryTypes.UPDATE });
-// };
+Product.update = function(id, product) {
+    return sequelize.query(`UPDATE products SET product_name = '${product.product_name}', price = ${product.price}, available_stock = ${product.available_stock}, category = '${product.category}', image_path = '${product.image_path}', product_description = '${product.product_description}', alt_text = '${product.alt_text}' WHERE product_id = ${id}`, { type: Sequelize.QueryTypes.UPDATE });
+};
 
 Product.delete = function(id) {
     return sequelize.query(`DELETE FROM products WHERE product_id = ${id}`, { type: Sequelize.QueryTypes.DELETE });
 };
+
+Product.getbyname = function(search_string){
+    return sequelize.query(`SELECT * FROM products WHERE product_name LIKE '%${search_string}%'`,{type: Sequelize.QueryTypes.SELECT});
+};
+
+
+
+Product.getbycategory = function(search_string){
+    return sequelize.query(`SELECT * FROM products WHERE category LIKE '%${search_string}%'`,{type: Sequelize.QueryTypes.SELECT});
+};
+
 
 module.exports = Product;
--- routes/api/products.js
+++ routes/api/products.js
@@ -28,16 +28,52 @@ router.get('/:id', async (req, res) => {
     }
 });
 
-// create a product in the database. increments product_id by 1.
+// create a product in the database. increments product_id by 1. (post)
 
 // update a product in the database.
-
+router.get('/:id', async (req, res) => {
+    try {
+        const product = await Product.update(req.params.id);
+        console.log("Product with id ", req.params.id, ": ", product);
+        res.json(product);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+    }
+});
 // delete a product in the database.
-
+router.delete('/:id', async (req, res) => {
+    try {
+        const product = await Product.delete(req.params.id);
+        console.log("Product with id ", req.params.id, ": ", product);
+        res.json(product);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+    }
+});
 // search for a product in the database by name.
-
+router.get('/search/product/:name', async (req, res) => {
+    try {
+        const product = await Product.getbyname(req.params.name);
+        //console.log("Product with id ", req.params.id, ": ", product);
+        res.json(product);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+    }
+});
 // search for a product in the database by category.
-
+router.get('/search/category/:name', async (req, res) => {
+    try {
+        const product = await Product.getbycategory(req.params.name);
+        //console.log("Product with id ", req.params.id, ": ", product);
+        res.json(product);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+    }
+});
 // get in-stock products in the database.
 
 // get products with price less than or equal to a given price.
